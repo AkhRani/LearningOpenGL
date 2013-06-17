d51ee9b235afe76b5f3dc87d26d305803408c4fb
Kevin Hart <kevin.kevinhart@gmail.com>
2013-06-16 18:35:13 -0700

    Added OpenGLDemo4, showing glDrawArrays
    Need to revert changes to OpenGLDemo3

---
+++ OpenGLDemo4/OpenGLDemo4.cpp
@@ -0,0 +1,79 @@
+/* Adding an orthographic projection to compensate for screen aspect ratio */
+#include "SDL.h"
+#include "SDL_opengl.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#undef main     // This un-does SDL's #define main
+
+bool initializeSdl()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("Unable to initialize SDL: %s\n", SDL_GetError());
+        return false;
+    }
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_Surface *screen = SDL_SetVideoMode(640, 480, 16, SDL_OPENGL);
+    if (!screen) {
+        printf("Unable to set video mode: %s\n", SDL_GetError());
+        return false;
+    }
+    return true;
+}
+
+void drawSquare()
+{
+    static const GLfloat squareCoords[] = {
+        -0.5f, 0.5f,   0.5f, 0.5f,   0.5f, -0.5f
+        ,0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f
+    };
+    glVertexPointer(2, GL_FLOAT, 0, squareCoords);
+
+    static const GLubyte squareColors[] = {
+        255, 0, 0
+        ,0, 255, 0
+        ,0, 0, 255
+        ,0, 0, 255
+        ,255, 255, 255
+        ,255, 0, 0
+    };
+    glColorPointer(3, GL_UNSIGNED_BYTE, 0, squareColors);
+
+    glDrawArrays(GL_TRIANGLES, 0, 12);
+}
+
+void drawSquareAt(double x, double y)
+{
+    GLdouble translationMatrix[] = {
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+        x,    y, 0.0f, 1.0f
+    };
+    glLoadMatrixd(translationMatrix);
+    drawSquare();
+}
+
+int main(int argc, char *argv[])
+{
+    if (initializeSdl()) {
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glEnableClientState(GL_COLOR_ARRAY);
+
+        glMatrixMode(GL_PROJECTION);
+        GLdouble ratio = 640.0f / 480.0f;
+        glOrtho(-ratio, ratio, -1, 1, -1, 1);
+        glMatrixMode(GL_MODELVIEW);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        drawSquareAt(0, 0);
+        drawSquareAt(0.5, 0.5);
+
+        SDL_GL_SwapBuffers();
+        SDL_Delay(3000);
+        SDL_Quit();
+    }
+    return 0;
+}
